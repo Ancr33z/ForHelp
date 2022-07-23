e7d33e0c18e9de7276685eda18ee371bebe5d974
Ancr33z <106481326+Ancr33z@users.noreply.github.com>
2022-07-23 17:12:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,29 +9,26 @@ document.querySelector(".left-switcher").addEventListener("click", function () {
 document
   .querySelector(".center-switcher")
   .addEventListener("click", function () {
-    offset = 560;
+    offset = 300;
     sliderLine.style.left = -offset + "px";
   });
 
 document
   .querySelector(".right-switcher")
   .addEventListener("click", function () {
-    offset = 1120;
+    offset = 617;
     sliderLine.style.left = -offset + "px";
   });
 
-
 const wrapObj = document.querySelector(".slider-switches");
 wrapObj.onclick = function (e) {
-  console.log(e.target.tagName)
-  if (e.target.tagName === 'IMG' ){
-  for (let i = 0; i < wrapObj.children.length ; i++) {
-    wrapObj.children[i].classList.remove("active");
-    
-  }
-  e.target.parentElement.classList.add("active");
+  console.log(e.target.tagName);
+  if (e.target.tagName === "IMG") {
+    for (let i = 0; i < wrapObj.children.length; i++) {
+      wrapObj.children[i].classList.remove("active");
+    }
+    e.target.parentElement.classList.add("active");
   } else {
-    return
-  } 
-
+    return;
+  }
 };
